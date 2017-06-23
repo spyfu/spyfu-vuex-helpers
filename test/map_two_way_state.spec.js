d6a2f3e5db0fab24944f91eb41bb6d992f472ab2
Scott Bedard <scottbedard90@gmail.com>
2017-06-22 19:00:28 -0700

    Scaffolding test utilities

---
+++ test/map_two_way_state.spec.js
@@ -0,0 +1,17 @@
+describe('mapTwoWayState', () => {
+    it.skip('normal syntax', () => {
+
+    });
+
+    it.skip('normal syntax (namespaced)', () => {
+
+    });
+
+    it.skip('shorthand', () => {
+
+    });
+
+    it.skip('shorthand (namespaced)', () => {
+
+    });
+});
