3b1dc33c09a5e94751ca8c04b0aabcd54a99565b
Scott Bedard <scottbedard90@gmail.com>
2017-06-22 20:52:39 -0700

    Minor cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
 import istanbul from 'rollup-plugin-istanbul';
+import uglify from 'rollup-plugin-uglify';
 
 let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
@@ -9,10 +10,12 @@ let plugins = [
     babel(babelrc()),
 ];
 
-if (process.env.BUILD !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
     plugins.push(istanbul({
         exclude: ['test/**/*', 'node_modules/**/*'],
     }));
+} else {
+    plugins.push(uglify());
 }
 
 export default {
