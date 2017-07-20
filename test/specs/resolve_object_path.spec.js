a678d408c2886c7c53a821cee66e9f1db7fd44de
Scott Bedard <scottbedard90@gmail.com>
2017-07-20 10:11:04 -0700

    Extract resolveObjectPath to it's own helper

---
+++ test/specs/resolve_object_path.spec.js
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import { resolveObjectPath } from '../../lib';
+
+describe('resolveObjectPath', () => {
+    const obj = {
+        foo: {
+            bar: {
+                baz: 'hello',
+            },
+        },
+    };
+
+    it('uses a dot as the default delimeter', () => {
+        expect(resolveObjectPath(obj, 'foo.bar.baz')).to.equal('hello');
+    });
+
+    it('returns undefined when path is not found', () => {
+        expect(resolveObjectPath(obj, 'one.two.three')).to.be.undefined;
+    });
+
+    it('accepts a string delimeter', () => {
+        expect(resolveObjectPath(obj, 'foo/bar/baz', '/')).to.equal('hello');
+    });
+
+    it('accepts a regex delimeter', () => {
+        expect(resolveObjectPath(obj, 'foo.bar/baz', /\.|\//)).to.equal('hello');
+    });
+});
