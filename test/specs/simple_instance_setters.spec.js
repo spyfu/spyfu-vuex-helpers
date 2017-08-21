df817c9bb6ac7b426ba79cacd644935b7ccae42b
Scott Bedard <scottbedard90@gmail.com>
2017-08-21 15:14:10 -0700

    Add tests for bad mutations

--- test/specs/simple_instance_setters.spec.js
+++ test/specs/simple_instance_setters.spec.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex';
+import sinon from 'sinon';
 import { expect } from 'chai';
 import { simpleInstanceSetters } from '../../lib';
 
@@ -58,5 +59,37 @@ describe('simpleInstanceSetters', () => {
 
         expect(store.state.instances[0].foo.bar).to.equal('hello');
         expect(store.state.instances[1].foo.bar).to.equal('baz');
+    });
+
+    it('logs a warning when the instance is not found', () => {
+        const warn = sinon.stub(console, 'warn');
+
+        const store = new Vuex.Store({
+            mutations: simpleInstanceSetters({
+                setFoo: 'foo',
+            }),
+            state: {
+                instances: [],
+            },
+        });
+
+        store.commit('setFoo', { id: 1, value: 'hello' });
+
+        expect(warn.called).to.be.true;
+
+        warn.restore();
+    });
+
+    it('throws an error when the payload is invalid', () => {
+        const store = new Vuex.Store({
+            mutations: simpleInstanceSetters({
+                setFoo: 'foo',
+            }),
+            state: {
+                instances: [{ id: 1, foo: '' }],
+            },
+        });
+
+        expect(() => store.commit('setFoo', { id: 1 })).to.throw;
     });
 });
