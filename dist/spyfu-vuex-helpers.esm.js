e0f913e1b291426f077e287614acd50cc23b9eb5
Scott Bedard <scottbedard90@gmail.com>
2017-08-21 13:55:52 -0700

    Dist

--- dist/spyfu-vuex-helpers.esm.js
+++ dist/spyfu-vuex-helpers.esm.js
@@ -24,7 +24,20 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 
 
+var defineProperty = function (obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
 
+  return obj;
+};
 
 
 
@@ -158,6 +171,70 @@ function stateAndPayloadAreValid(config, state, payload) {
     return true;
 }
 
+// Similar to Object.entries but without using polyfill
+var getEntries = function (obj) {
+    return Object.keys(obj).map(function (key) {
+        return [key, obj[key]];
+    });
+};
+
+// Function to compose other functions (right to left evaluation)
+var compose = function () {
+    var fns = arguments;
+
+    return function () {
+        var result = void 0;
+
+        for (var i = fns.length - 1; i > -1; i--) {
+            if (i === fns.length - 1) {
+                result = fns[i].apply(fns[i], arguments);
+            } else {
+                result = fns[i].call(this, result);
+            }
+        }
+
+        return result;
+    };
+};
+
+// Convert KeyValuePair[] to Object
+var toObject = function (obj, keyValuePair) {
+    var _keyValuePair = slicedToArray(keyValuePair, 2),
+        key = _keyValuePair[0],
+        value = _keyValuePair[1];
+
+    obj[key] = value;
+
+    return obj;
+};
+
+// Create a wrapper function which invokes the original function
+// passing in `this.id`
+var wrapGetterFn = function wrapGetterFn(_ref) {
+    var _ref2 = slicedToArray(_ref, 2),
+        key = _ref2[0],
+        originalFn = _ref2[1];
+
+    var newFn = function newFn() {
+        var innerFn = originalFn.apply(this, arguments);
+
+        if (typeof innerFn !== 'function') {
+            /* istanbul ignore next */
+            throw 'The getter ' + key + ' does not return a function. Try using the \'mapGetter\' helper instead';
+        }
+
+        return innerFn(this.id);
+    };
+
+    return [key, newFn];
+};
+
+function invokeGettersWithId(getters) {
+    return getEntries(getters).map(wrapGetterFn).reduce(toObject, {});
+}
+
+var mapInstanceGetters = compose(invokeGettersWithId, mapGetters);
+
 /**
  * Helper function for resolving nested object values.
  *
@@ -263,74 +340,101 @@ function createSetter(namespace, mappings) {
     };
 }
 
-// Similar to Object.entries but without using polyfill
-var getEntries = function (obj) {
-    return Object.keys(obj).map(function (key) {
-        return [key, obj[key]];
-    });
-};
+/**
+ * Simple mutations that set an instance's state equal to a value.
+ *
+ * @param  {Object}
+ * @param  {String}
+ * @param  {String}
+ * @return {Object}
+ */
+var simpleInstanceSetters = function (setters) {
+    var stateKey = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'instances';
+    var instanceKey = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'id';
 
-// Function to compose other functions (right to left evaluation)
-var compose = function () {
-    var fns = arguments;
+    // loop over the setter keys and make a mutation for each
+    return Object.keys(setters).reduce(function (mutations, name) {
 
-    return function () {
-        var result = void 0;
+        // attach our new mutation to result
+        return Object.assign({}, mutations, defineProperty({}, name, function (state, payload) {
+            // find the instance that we're mutating
+            var instance = findInstance(state, stateKey, instanceKey, payload);
 
-        for (var i = fns.length - 1; i > -1; i--) {
-            if (i === fns.length - 1) {
-                result = fns[i].apply(fns[i], arguments);
+            if (instance) {
+                var value = findValue(payload, instanceKey);
+
+                // if the setter name has a dot, then resolve the
+                // state path before feeding our value into it.
+                if (setters[name].indexOf('.') > -1) {
+                    var obj = setters[name].split('.');
+                    var key = obj.pop();
+
+                    resolveObjectPath(instance, obj)[key] = value;
+                } else {
+                    // otherwise, just set the instance state to our value
+                    instance[setters[name]] = value;
+                }
             } else {
-                result = fns[i].call(this, result);
+                // if the instance wasn't found, let the dev know with a warning
+                console.warn('An instance with an identifier of ' + instanceKey + ' was not found.');
             }
-        }
-
-        return result;
-    };
+        }));
+    }, {});
 };
 
-// Convert KeyValuePair[] to Object
-var toObject = function (obj, keyValuePair) {
-    var _keyValuePair = slicedToArray(keyValuePair, 2),
-        key = _keyValuePair[0],
-        value = _keyValuePair[1];
-
-    obj[key] = value;
-
-    return obj;
-};
-
-// Create a wrapper function which invokes the original function
-// passing in `this.id`
-var wrapGetterFn = function wrapGetterFn(_ref) {
-    var _ref2 = slicedToArray(_ref, 2),
-        key = _ref2[0],
-        originalFn = _ref2[1];
-
-    var newFn = function newFn() {
-        var innerFn = originalFn.apply(this, arguments);
-
-        if (typeof innerFn !== 'function') {
-            /* istanbul ignore next */
-            throw 'The getter ' + key + ' does not return a function. Try using the \'mapGetter\' helper instead';
-        }
-
-        return innerFn(this.id);
-    };
-
-    return [key, newFn];
-};
-
-function invokeGettersWithId(getters) {
-    return getEntries(getters).map(wrapGetterFn).reduce(toObject, {});
+// helper function to find the correct instance
+function findInstance(state, stateKey, instanceKey, payload) {
+    return state[stateKey].find(function (obj) {
+        return obj[instanceKey] === payload[instanceKey];
+    });
 }
 
-var mapInstanceGetters = compose(invokeGettersWithId, mapGetters);
+// helper function to find the payload value
+function findValue(payload, instanceKey) {
+    for (var key in payload) {
+        if (key !== instanceKey) {
+            return payload[key];
+        }
+    }
+
+    // if we don't have a value, throw an error because the payload is invalid.
+    throw new Error('Failed to mutate instance, no value found in payload.', payload);
+}
+
+/**
+ * Simple mutations that set a piece of state equal to a value.
+ *
+ * @param  {Object} setters     Object mapping mutations to state
+ * @return {Object}
+ */
+var simpleSetters = function (setters) {
+    // loop over the setter keys and make a mutation for each
+    return Object.keys(setters).reduce(function (mutations, name) {
+
+        // attach our new mutation to result
+        return Object.assign({}, mutations, defineProperty({}, name, function (state, value) {
+
+            // if the setter name has a dot, then resolve the
+            // state path before feeding our value into it.
+            if (setters[name].indexOf('.') > -1) {
+                var obj = setters[name].split('.');
+                var key = obj.pop();
+
+                resolveObjectPath(state, obj)[key] = value;
+            }
+
+            // otherwise, just set the state to our value
+            else state[setters[name]] = value;
+        }));
+    }, {});
+};
 
 module.exports = {
     findInstanceThen: findInstanceThen,
+    mapInstanceGetters: mapInstanceGetters,
     mapTwoWayState: mapTwoWayState,
     resolveObjectPath: resolveObjectPath,
-    mapInstanceGetters: mapInstanceGetters
+    simpleInstanceSetters: simpleInstanceSetters,
+    simpleSetters: simpleSetters
 };
 //# sourceMappingURL=spyfu-vuex-helpers.esm.js.map
