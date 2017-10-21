f5570f68625ee0198320b32826870415d6f448aa
scottbedard <scottbedard90@gmail.com>
2017-10-20 22:52:41 -0700

    adding a few tests for object syntax

--- test/specs/map_instance_state.spec.js
+++ test/specs/map_instance_state.spec.js
@@ -9,23 +9,30 @@ import Vuex from 'vuex';
 const mount = function(vm) {
     const store = new Vuex.Store({
         modules: {
-            test2: {
-                state: {
-                    instances: [{ id: 2, foo: 'bar' }],
-                },
-            },
-            test3: {
+            namespaced: {
                 modules: {
                     foo: {
                         state: {
-                            instances: [{ id: 3, foo: 'bar' }],
-                        }
-                    }
-                }
-            }
+                            instances: [
+                                { id: 3, value: 'test3' },
+                                { id: 5, value: 'test5' },
+                            ],
+                        },
+                    },
+                },
+                state: {
+                    instances: [
+                        { id: 2, value: 'test2' },
+                        { id: 4, value: 'test4' },
+                    ],
+                },
+            },
         },
         state: {
-            instances: [{ id: 1, foo: 'bar' }],
+            instances: [
+                { id: 1, value: 'test1' },
+                { id: 4, value: 'test4' },
+            ],
         },
     });
 
@@ -47,11 +54,11 @@ describe.only('mapInstanceState', () => {
         const vm = mount({
             computed: {
                 id: () => 1,
-                ...mapInstanceState(['foo']),
+                ...mapInstanceState(['value']),
             },
         });
 
-        expect(vm.foo).to.equal('bar');
+        expect(vm.value).to.equal('test1');
     });
 
     // test 2
@@ -59,21 +66,58 @@ describe.only('mapInstanceState', () => {
         const vm = mount({
             computed: {
                 id: () => 2,
-                ...mapInstanceState('test2', ['foo']),
+                ...mapInstanceState('namespaced', ['value']),
             },
         });
 
-        expect(vm.foo).to.equal('bar');
+        expect(vm.value).to.equal('test2');
     });
 
+    // test 3
     it('array syntax, nested namespace', () => {
         const vm = mount({
             computed: {
                 id: () => 3,
-                ...mapInstanceState('test3/foo', ['foo']),
+                ...mapInstanceState('namespaced/foo', ['value']),
             },
         });
 
-        expect(vm.foo).to.equal('bar');
-    })
+        expect(vm.value).to.equal('test3');
+    });
+
+    // test 4
+    it('object syntax', () => {
+        const vm = mount({
+            computed: {
+                id: () => 4,
+                ...mapInstanceState({ foo: 'value' }),
+            }
+        });
+
+        expect(vm.foo).to.equal('test4');
+    });
+
+    // test 4
+    it('object syntax, namespaced', () => {
+        const vm = mount({
+            computed: {
+                id: () => 4,
+                ...mapInstanceState('namespaced', { foo: 'value' }),
+            }
+        });
+
+        expect(vm.foo).to.equal('test4');
+    });
+
+    // test 5
+    it('object syntax, nested namespace', () => {
+        const vm = mount({
+            computed: {
+                id: () => 5,
+                ...mapInstanceState('namespaced/foo', { foo: 'value' }),
+            }
+        });
+
+        expect(vm.foo).to.equal('test5');
+    });
 });
