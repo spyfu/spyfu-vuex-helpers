51a9a970dfd9bde84ed014d7c57ac9ca854ac051
scottbedard <scottbedard90@gmail.com>
2017-10-20 22:43:44 -0700

    flesh out array syntax a bit

--- test/specs/map_instance_state.spec.js
+++ test/specs/map_instance_state.spec.js
@@ -8,18 +8,33 @@ import Vuex from 'vuex';
 //
 const mount = function(vm) {
     const store = new Vuex.Store({
-        state: {
-            instances: [
-                { id: 1, foo: 'bar' },
-            ],
+        modules: {
+            test2: {
+                state: {
+                    instances: [{ id: 2, foo: 'bar' }],
+                },
+            },
+            test3: {
+                modules: {
+                    foo: {
+                        state: {
+                            instances: [{ id: 3, foo: 'bar' }],
+                        }
+                    }
+                }
+            }
         },
-        });
+        state: {
+            instances: [{ id: 1, foo: 'bar' }],
+        },
+    });
 
-    return new Vue(Object.assign({
+    return new Vue({
         el: document.createElement('div'),
         render: h => h('div'),
         store,
-    }, vm));
+        ...vm,
+    });
 }
 
 //
@@ -38,4 +53,27 @@ describe.only('mapInstanceState', () => {
 
         expect(vm.foo).to.equal('bar');
     });
+
+    // test 2
+    it('array syntax, namespaced', () => {
+        const vm = mount({
+            computed: {
+                id: () => 2,
+                ...mapInstanceState('test2', ['foo']),
+            },
+        });
+
+        expect(vm.foo).to.equal('bar');
+    });
+
+    it('array syntax, nested namespace', () => {
+        const vm = mount({
+            computed: {
+                id: () => 3,
+                ...mapInstanceState('test3/foo', ['foo']),
+            },
+        });
+
+        expect(vm.foo).to.equal('bar');
+    })
 });
