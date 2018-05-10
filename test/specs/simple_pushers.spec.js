aad2be5e6622f09979844dc30bcfcd2777c7e05c
scottbedard <scottbedard90@gmail.com>
2018-05-09 23:21:28 -0700

    add dev errors for simplePushers

--- test/specs/simple_pushers.spec.js
+++ test/specs/simple_pushers.spec.js
@@ -36,5 +36,128 @@ describe('simplePushers', () => {
 
         store.commit('pushBaz', 'hello');
         expect(store.state.foo.bar.baz).to.deep.equal(['hello']);
+    });
+
+    it('throws an error when target is not found', () => {
+        let caught = false;
+
+        try {
+            const store = new Vuex.Store({
+                mutations: simplePushers({
+                    pushFoo: 'bar',
+                }),
+                state: {},
+            });
+
+            store.commit('pushFoo', 'whatever');
+        } catch (e) {
+            caught = true;
+
+            if (e.message.startsWith('[spyfu-vuex-helpers]') === false) {
+                throw e;
+            }
+        }
+        
+        expect(caught).to.be.true;
+    });
+
+    it('throws an error when target is not an array', () => {
+        let caught = false;
+
+        try {
+            const store = new Vuex.Store({
+                mutations: simplePushers({
+                    pushFoo: 'bar',
+                }),
+                state: {
+                    bar: 'not an array',
+                },
+            });
+
+            store.commit('pushFoo', 'whatever');
+        } catch (e) {
+            caught = true;
+
+            if (e.message.startsWith('[spyfu-vuex-helpers]') === false) {
+                throw e;
+            }
+        }
+
+        expect(caught).to.be.true;
+    });
+
+    it('throws an error when a nested path parent is not found', () => {
+        let caught = false;
+
+        try {
+            const store = new Vuex.Store({
+                mutations: simplePushers({
+                    pushBaz: 'non.existant.whatever',
+                }),
+                state: {},
+            });
+
+            store.commit('pushBaz', 'whatever');
+        } catch (e) {
+            caught = true;
+
+            if (e.message.startsWith('[spyfu-vuex-helpers]') === false) {
+                throw e;
+            }
+        }
+
+        expect(caught).to.be.true;
+    });
+
+    it('throws an error when a nested path is not found', () => {
+        let caught = false;
+
+        try {
+            const store = new Vuex.Store({
+                mutations: simplePushers({
+                    pushBaz: 'foo.bar',
+                }),
+                state: {
+                    foo: {},
+                },
+            });
+
+            store.commit('pushBaz', 'whatever');
+        } catch (e) {
+            caught = true;
+
+            if (e.message.startsWith('[spyfu-vuex-helpers]') === false) {
+                throw e;
+            }
+        }
+
+        expect(caught).to.be.true;
+    });
+
+    it('throws an error if the nested path is not an array', () => {
+        let caught = false;
+
+        try {
+            const store = new Vuex.Store({
+                mutations: simplePushers({
+                    pushBaz: 'foo.bar',
+                }),
+                state: {
+                    foo: {
+                        bar: 'not an array',
+                    },
+                },
+            });
+
+            store.commit('pushBaz', 'whatever');
+        } catch (e) {
+            caught = true;
+
+            if (e.message.startsWith('[spyfu-vuex-helpers]') === false) {
+                throw e;
+            }
+        }
+
+        expect(caught).to.be.true;
     })
 });
